5a59346824564736819361e42cbb754797407222
Yogesh Jaga <70561287+y-jaga@users.noreply.github.com>
2023-05-14 23:24:08 +0530

    Update index.js
    Added functionality to button to roll the dice.

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-/*RANDOM NUMBER generation b/w 1 to 6 for dice1 */
+/*Function to generate RANDOM NUMBER b/w 1 to 6 for dice1 a */
+var randomNumber1, randomNumber2;
+function dice1RandomNumber() {
+  randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomNumber1 = Math.random();
-randomNumber1 = randomNumber1 * 6;
-randomNumber1 = Math.floor(randomNumber1);
-randomNumber1 = randomNumber1 + 1;
+  /*Images of dice1 changes*/
+  document
+    .querySelector(".img1")
+    .setAttribute("src", `images/dice${randomNumber1}.png`);
+}
 
-/*Images of dice1 changes*/
-document
-  .querySelector(".img1")
-  .setAttribute("src", `images/dice${randomNumber1}.png`);
+//RANDOM NUMBER generation b/w 1 to 6 for dice1
+function dice2RandomNumber() {
+  randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
-/*RANDOM NUMBER generation b/w 1 to 6 for dice1 */
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-document.querySelector(".img2").setAttribute("src", randomImageSource2);
+  // Images of dice1 changes
+  document.querySelector(".img2").setAttribute("src", randomImageSource2);
+}
 
-if (randomNumber1 === randomNumber2) {
-  document.querySelector("h1").innerText = "Draw!";
-} else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerText = "Player 2 Wins!🚩";
-} else {
-  document.querySelector("h1").innerText = "🚩Player 1 Wins!";
+// Adding Event Listener to dice1 and dice2
+var button = document.querySelector(".btn");
+button.addEventListener("click", dice1RandomNumber);
+button.addEventListener("click", dice2RandomNumber);
+button.addEventListener("click", compare);
+
+function compare() {
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerText = "Draw!";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerText = "Player 2 Wins!🚩";
+  } else {
+    document.querySelector("h1").innerText = "🚩Player 1 Wins!";
+  }
 }
